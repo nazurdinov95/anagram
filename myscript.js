dd8c1759bae1c905417849968775a6be9e785b37
amr <nazurdinov95>
2019-12-16 14:00:58 +0500

    add anagram function

--- myscript.js
+++ myscript.js
@@ -1,4 +1,31 @@
-function anagramStr(str) {
-  return str.split("").reverse().join("");
+isAnagram('silent', 'listen');
+
+function isAnagram(stringA, stringB) {
+
+  function createCharMap(text) {
+    let charMap = {}
+
+    for (let char of text) {
+      if (charMap.hasOwnProperty(char)) {
+        charMap[char]++
+      } else {
+        charMap[char] = 1
+      }
+    }
+
+    return charMap
+  }
+
+  if (stringA.length === stringB.length) {
+    let stringAMap = createCharMap(stringA)
+    let stringBMap = createCharMap(stringB)
+    for (let char in stringAMap) {
+      if (stringAMap[char] !== stringBMap[char]) {
+        return false
+      }
+    }
+    return true
+  } else {
+    return false
+  }
 }
-alert(anagramStr("Hello world!"));
